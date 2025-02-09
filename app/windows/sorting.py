ebeec6ba40d88fff6c54b8cb572897862ef47002
Javier Iregui <iregui.javi@gmail.com>
2025-02-09 13:53:41 +0100

    Sorting UI done

--- app/windows/sorting.py
+++ app/windows/sorting.py
@@ -17,44 +17,55 @@ class SortingWindow:
         self.win_height = 400
         
         self.win_x, self.win_y = calculate_window_position(root, self.win_width, self.win_height, offset)
-
         self.window.geometry(f"{self.win_width}x{self.win_height}+{self.win_x}+{self.win_y}")
         
         # Styles
         self.create_theme()
         style = ttk.Style()
         style.theme_use("myTheme")
+        
+        # Grid configuration
+        self.window.grid_rowconfigure(1, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
 
-        # Main Frame
-        self.frame = tk.Frame(self.window, bg="#303030")
-        self.frame.pack(fill=tk.X, padx=25, pady=20)
+        # Top Frame
+        self.top_frame = tk.Frame(self.window, bg="#303030")
+        self.top_frame.grid(row=0, column=0, padx=25, pady=20, sticky="ew")
         
         # Label
-        ttk.Label(self.frame, text=algorithm_name, style="Title.TLabel").pack(side=tk.LEFT)
+        ttk.Label(self.top_frame, text=algorithm_name, style="Title.TLabel").pack(side=tk.LEFT)
         
         # Learn more button
-        ttk.Button(self.frame, text="Learn more", style="Play.TButton", command=self.show_algorithm_info).pack(side=tk.LEFT, padx=20)
+        ttk.Button(self.top_frame, text="Learn more", style="Play.TButton", command=self.show_algorithm_info).pack(side=tk.LEFT, padx=20)
+        self.description_open = False
         
         # Item counter
         self.n = tk.StringVar(value="15")
         self.n.trace_add("write", self.validate_n)
         
-        self.item_count_entry = tk.Entry(self.frame, font=("Helvetica", 12), width=5, textvariable=self.n)
+        self.item_count_entry = tk.Entry(self.top_frame, font=("Helvetica", 12), justify="center", insertbackground="#FFFFFF", borderwidth=0, border=0, highlightthickness=0, bg="#404040", fg="#FFFFFF", relief="flat", width=5, textvariable=self.n)
         self.item_count_entry.pack(side=tk.RIGHT)
-        tk.Label(self.frame, text="n:", font=("Helvetica", 12), fg="#FFFFFF", bg="#303030").pack(side=tk.RIGHT, padx=10)
+        tk.Label(self.top_frame, text="n:", font=("Helvetica", 12), fg="#FFFFFF", bg="#303030").pack(side=tk.RIGHT, padx=5)
         
         # Canvas
-        self.canvas = tk.Canvas(self.window, bg="#404040")
-        self.canvas.pack(fill=tk.BOTH, padx=25, expand=True)
-        
-        # Frame inferior con temporizador y botón play/stop
+        self.canvas = tk.Canvas(self.window, bg="#404040", highlightthickness=0, border=0, borderwidth=0, relief="flat")
+        self.canvas.grid(row=1, column=0, padx=25, sticky="nsew")
+          
+        # Bottom frame
         bottom_frame = tk.Frame(self.window, bg="#303030")
-        bottom_frame.pack(fill=tk.X, padx=20, pady=10)
+        bottom_frame.grid(row=2, column=0, padx=25, pady=20, sticky="ew")
         
-        self.timer_label = tk.Label(bottom_frame, text="0.00s", font=("Helvetica", 14), fg="#FFFFFF", bg="#303030")
+        # Reset button
+        self.reset_button = ttk.Button(bottom_frame, text="Reset", style="Play.TButton", command=self.reset)
+        self.reset_button.pack(side=tk.LEFT, padx=10)
+        
+        # Timer
+        self.timer_label = tk.Label(bottom_frame, text="0.000s", font=("Helvetica", 14), fg="#FFFFFF", bg="#303030")
         self.timer_label.pack(side=tk.RIGHT)
         
-        self.play_button = ttk.Button(bottom_frame, text="Play", style="Play.TButton", command=self.toggle_algorithm).pack(side=tk.RIGHT, padx=10)
+        # Play button
+        self.play_button = ttk.Button(bottom_frame, text="Play", style="Play.TButton", command=self.toggle_algorithm)
+        self.play_button.pack(side=tk.RIGHT, padx=10)
 
         self.is_running = False
         self.start_time = 0
@@ -62,38 +73,46 @@ class SortingWindow:
         self.update_timer()
 
     def show_algorithm_info(self):
-        # Crear una pequeña ventana emergente con la explicación del algoritmo
-        info_window = tk.Toplevel(self.window)
-        info_window.title("Explicación del Algoritmo")
-        info_window.geometry("300x200")
-        
-        explanation = "Aquí va la explicación del algoritmo.\nPuede ser tan detallada como quieras."
-        
-        info_label = tk.Label(info_window, text=explanation, font=("Helvetica", 12), fg="#000000", padx=20, pady=20)
-        info_label.pack()
+        if not self.description_open:
+            self.description_open = True
+            info_window = tk.Toplevel(self.window)
+            info_window.title("Learn more")
+            info_window.geometry("300x200")
+            info_window.geometry(f"{300}x{200}+{self.win_x + self.win_width // 2 - 150}+{self.win_y + self.win_height // 2 - 100}")
+            info_window.resizable(False, False)
+            info_window.configure(bg="#303030")
+            
+            explanation = "Algorithm Explained"
+            
+            info_label = tk.Label(info_window, text=explanation, font=("Helvetica", 10), bg="#303030", fg="#FFFFFF", padx=10, pady=10)
+            info_label.pack()
+            
+            info_window.protocol("WM_DELETE_WINDOW", lambda: self.close_info_window(info_window))
 
-        close_button = tk.Button(info_window, text="Cerrar", command=info_window.destroy)
-        close_button.pack(pady=10)
-
+    def close_info_window(self, window):
+        self.description_open = False
+        window.destroy()
+    
     def validate_n(self, *args):
+        self.reset()
         if self.n.get() != "":
             try:
                 value = int(self.n.get())
                 if value < 1 or value > 250:
                     raise ValueError
             except ValueError:
-                self.n.set("15")
-            
+                self.n.set("15") 
     
     def toggle_algorithm(self):
         if self.n.get() != "":
             if self.is_running:
+                self.play_button.config(text="Play")
                 self.is_running = False
             else:
+                self.play_button.config(text="Stop")
                 self.is_running = True
-                self.start_time = time.time() - self.elapsed_time  # Continuar desde donde lo dejamos
+                self.start_time = time.time() - self.elapsed_time
             
-            # Aquí podrías empezar o detener el algoritmo real de ordenamiento
             self.update_timer()
         else:
             self.item_count_entry.focus_force()
@@ -101,8 +120,17 @@ class SortingWindow:
     def update_timer(self):
         if self.is_running:
             self.elapsed_time = time.time() - self.start_time
-            self.timer_label.config(text=f"{self.elapsed_time:.2f}s")
-        self.window.after(100, self.update_timer)  # Actualiza el temporizador cada 100ms
+            self.timer_label.config(text=f"{self.elapsed_time:.3f}s")
+        self.window.after(10, self.update_timer)
+        
+    def reset(self):
+        self.is_running = False
+        self.play_button.config(text="Play")
+        
+        self.elapsed_time = 0
+        self.timer_label.config(text=f"{self.elapsed_time:.3f}s")
+        
+        self.update_timer()
         
     def create_theme(self):
         style = ttk.Style()
